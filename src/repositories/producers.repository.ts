347920b462bc7a34b177106ba499197af3a5b901
Paulo Vaz <pvaz1006@gmail.com>
2024-08-20 20:46:44 -0300

    Update producers.repository.ts

--- src/repositories/producers.repository.ts
+++ src/repositories/producers.repository.ts
@@ -4,22 +4,30 @@ import { IUserRepository } from "../modules/producers/interfaces/Iuser.interface
 import { AppDataSource } from "../db/data-source";
 
 export class ProducersRepository implements IUserRepository {
-  private repository: Repository<ProducersEntity>;
+  private repository: Repository<ProducersEntity> = AppDataSource.getRepository(ProducersEntity);
 
-  constructor() {
-    this.repository = AppDataSource.getRepository(ProducersEntity)
+  private async findById(id) {
+    return this.repository.find({
+      where: {
+        id,
+      }
+    })
   }
 
   async save(payload) {
-    console.log(payload)
-    return 'ok' //this.repository.save(payload)
+    return this.repository.save(payload)
   }
 
   async update(id, payload) {
-    return 'ok'
+    const searchProducer = await this.findById(id);
+    const info = {
+      ...searchProducer,
+      ...payload
+    }
+    return await this.save(info)
   }
 
-  async delete(id, payload) {
-    return 'ok'
+  async delete(id) {
+    return await this.repository.delete(id)
   }
 }
