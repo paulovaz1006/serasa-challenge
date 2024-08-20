a3d7d4dafbb14efe149fe27e0e67ae5fcdd3dac9
Paulo Vaz <pvaz1006@gmail.com>
2024-08-20 20:30:38 -0300

    Update cultures.repository.ts

--- src/repositories/cultures.repository.ts
+++ src/repositories/cultures.repository.ts
@@ -3,23 +3,27 @@ import { IUserRepository } from "../modules/producers/interfaces/Iuser.interface
 import { AppDataSource } from "../db/data-source";
 import { CulturesEntity } from "../entity/Cutlures.entity";
 
-export class CulturesRepository implements IUserRepository {
-  private repository: Repository<CulturesEntity>;
+export class CulturesRepository implements ICultureRepository {
+  private repository: Repository<CulturesEntity> = AppDataSource.getRepository(CulturesEntity);
 
-  constructor() {
-    this.repository = AppDataSource.getRepository(CulturesEntity)
+  private async findById(id) {
+    return this.repository.find({
+      where: {
+        id,
+      }
+    })
   }
 
   async save(payload) {
-    console.log(payload)
-    return 'ok' //this.repository.save(payload)
+    return this.repository.save(payload)
   }
 
   async update(id, payload) {
-    return 'ok'
-  }
-
-  async delete(id, payload) {
-    return 'ok'
+    const searchProducer = await this.findById(id);
+    const info = {
+      ...searchProducer,
+      ...payload
+    }
+    return await this.save(info)
   }
 }
