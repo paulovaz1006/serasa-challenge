f5b0ab1c91c83ad03878d6737ffe212e8631cbbb
Paulo Vaz <pvaz1006@gmail.com>
2024-08-20 20:30:49 -0300

    Update farms.repository.ts

--- src/repositories/farms.repository.ts
+++ src/repositories/farms.repository.ts
@@ -4,20 +4,29 @@ import { AppDataSource } from "../db/data-source";
 import { FarmsEntity } from "../entity/Farms.entity";
 
 export class FarmsRepository implements IUserRepository {
-  private repository: Repository<FarmsEntity>;
+  private repository: Repository<FarmsEntity> = AppDataSource.getRepository(FarmsEntity);
 
-  constructor() {
-    this.repository = AppDataSource.getRepository(FarmsEntity)
+  private async findById(id) {
+    return this.repository.find({
+      where: {
+        id,
+      }
+    })
   }
 
   async save(payload) {
-    console.log(payload)
-    return 'ok' //this.repository.save(payload)
+    return this.repository.save(payload)
   }
 
   async update(id, payload) {
-    return 'ok'
+    const searchProducer = await this.findById(id);
+    const info = {
+      ...searchProducer,
+      ...payload
+    }
+    return await this.save(info)
   }
+
 
   async delete(id, payload) {
     return 'ok'
