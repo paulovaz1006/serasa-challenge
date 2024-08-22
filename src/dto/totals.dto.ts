da6b0710f76108bc1a6c0ccfe513f0bc6ef69f4f
Paulo Vaz <pvaz1006@gmail.com>
2024-08-22 03:01:10 -0300

    feat: add dto

---
+++ src/dto/totals.dto.ts
@@ -0,0 +1,13 @@
+export class TotalsDTO {
+  producer_id: string;
+  producer_name: string;
+  farm_id: string;
+  farm_name: string;
+  area_total_hectares: number;
+  area_vegetation_hectares: number;
+  area_agriculture_hectares: number;
+
+  constructor() {
+
+  }
+}
