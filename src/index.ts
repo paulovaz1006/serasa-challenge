a372f106bfa629c40c8c9e5e42f44bea8b02b548
Paulo Vaz <pvaz1006@gmail.com>
2024-08-22 03:03:00 -0300

    feat: add index

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
 import { AppDataSource } from "./db/data-source"
 import express from 'express';
 import routes from './routes';
+import { setupSwagger } from "./config/swagger";
+import dotenv, { config } from "dotenv";
+import cors from 'cors';
 
 AppDataSource.initialize().then(async () => {
     const app = express()
-
     app.use(express.json());
+    config()
+    app.use(cors())
     app.use(routes)
+    setupSwagger(app);
     app.listen(3000, () => {
         console.log("run server")
     })
